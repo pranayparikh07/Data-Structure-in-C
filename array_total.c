87a3fe0b27cdedfe24416a22af1680b6edc8e99b
Parikh Pranay Himanshubhai <pranayparikh07@gmail.com>
2026-06-28 08:33:56 +0530

    array total

--- array_total.c
+++ array_total.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
 int main(){
     int a[10], sum=0;
-    for(int i=0;i<=10;i++){
+    
+    for(int i=0;i<10;i++){
         printf("Enter element for a[%d]: ", i);
         scanf("%d",&a[i]);
         sum += a[i];
     }
-    printf("The sum of all 10 values is: %d\n", sum);
+    printf("The Final Array is: \n");
+
+    for(int i=0;i<10;i++){
+        printf(" %d", a[i]);
+    }
+    printf("\nThe sum of all 10 values is: %d\n", sum);
     return 0;
 }
